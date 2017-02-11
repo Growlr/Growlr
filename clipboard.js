3e5cfba15acf7ac811062ac394be4296321d2d0c
Jon Low <lowjonc@gmail.com>
2017-02-10 17:32:24 -0700

    redux is a go

---
+++ clipboard.js
@@ -0,0 +1,14 @@
+<View style={styles.container}>
+  <Text style={styles.welcome}>
+     --- GROWLRRR ---
+  </Text>
+
+  <Text style={styles.instructions}>
+    grrr
+  </Text>
+
+  <Text style={styles.instructions}>
+    Press Cmd+R to launch nukes at China,{'\n'}
+    Cmd+D or shake for dev menu
+  </Text>
+</View>
