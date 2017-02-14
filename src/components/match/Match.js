f3984fd80fabc6ce6fba3cdf97bc3ae2421b2c37
Garrett Duff <garrettduff@Garretts-MacBook-Pro.local>
2017-02-14 12:06:41 -0700

    Changed one of the routes on main view and styles on Match View

--- src/components/match/Match.js
+++ src/components/match/Match.js
@@ -12,22 +12,36 @@ class Match extends Component {
     render() {
         return (
           <View>
-            <Text> Main</Text>
-            <Text> Main</Text>
-            <Text> Main</Text>
-            <Text> Main</Text>
-            <Text onPress={Actions.landingPage}> Landing</Text>
-            <Text onPress={Actions.profileView}> Profile</Text>
-            <Text onPress={Actions.mainView}> Main</Text>
-            <Text onPress={Actions.matchView}> Match</Text>
-            <Text onPress={Actions.settingsView}> Settings</Text>
+              <Text> Main</Text>
+              <Text> Main</Text>
+              <Text> Main</Text>
+              <Text> Main</Text>
+              <Text onPress={Actions.landingPage}> Landing</Text>
+              <Text onPress={Actions.profileView}> Profile</Text>
+              <Text onPress={Actions.mainView}> Main</Text>
+              <Text onPress={Actions.matchView}> Match</Text>
+              <Text onPress={Actions.settingsView}> Settings</Text>
+
+            <View>
+              <TextInput style={{height: 30, width: 350, borderColor: 'gray', borderWidth: 1, borderRadius: 5, backgroundColor: '#ddd'}}
+              onChangeText={(text) => this.setState({text})}
+              value={this.props.text}/>
+            </View>
 
             <View>
               <Image
-                style={{width: 75, height: 75, margin: 20, borderRadius: 40}}
+                style={{width: 75, height: 75, margin: 15, borderRadius: 40}}
                 source={{uri: 'https://pbs.twimg.com/profile_images/1634021833/50258_211053130451_5559002_n_400x400.jpg'}} />
 
-              <Text style={{marginLeft: 20}}>Message message message message</Text>
+              <Text style={{marginLeft: 110, position: 'absolute', top: 30}}>Name</Text>
+            </View>
+
+            <View>
+              <Image
+                style={{width: 75, height: 75, margin: 15, borderRadius: 40}}
+                source={{uri: 'https://yt3.ggpht.com/-iMljpF2Y-CQ/AAAAAAAAAAI/AAAAAAAAAAA/1-Uj5vogKcM/s900-c-k-no-mo-rj-c0xffffff/photo.jpg'}} />
+
+              <Text style={{marginLeft: 110, position: 'absolute', top: 30}}>Name</Text>
             </View>
           </View>
 
