ffe5c3bb726b827a66293285c99c34034156d58c
jacoblemperle <jacob.lemperle@gmail.com>
2017-02-21 10:32:53 -0700

    add pet query

---
+++ db/add_pet.sql
@@ -0,0 +1,3 @@
+INSERT INTO pet_data (source_link, img_link, pet_id, gender, breed, color, age, kennel_id, kennel_status, description, pet_name, owner_id)
+values
+($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
