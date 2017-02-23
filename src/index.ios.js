f4e875385fc17a0c00efbd05487fda7f281d92b6
Garrett Duff <garrettduff@gmail.com>
2017-02-23 14:55:34 -0700

    Fixed merge conflicts

--- src/index.ios.js
+++ src/index.ios.js
@@ -11,13 +11,9 @@ import Profile from './components/profile/Profile'
 import Match from './components/match/Match'
 import Settings from './components/settings/Settings'
 import Login from './components/main/Login'
-<<<<<<< HEAD
 import NavBar from './components/navbar/NavBar';
 import NavStyles from './components/navbar/NavStyles';
-=======
-import NavBar from './components/navbar/NavBar'
 import Owner from './components/owner/Owner'
->>>>>>> master
 import {
   AppRegistry,
   StyleSheet,
@@ -35,16 +31,13 @@ export default class Growlr extends Component {
             <Scene key="root" >
 
 
-<<<<<<< HEAD
               <Scene key="mainView" navBar={NavBar} navigationBarStyle={{position: 'absolute', top: 0}} panHandler={null} sceneStyle={{ backgroundColor: 'lightgray'}} component={Main} title="Discover Pets" initial={true}/>
               <Scene key="landingPage" component={Landing} title="Landing Page" />
               <Scene key="profileView" navBar={NavBar} component={Profile} title="Your Profile"/>
-=======
               <Scene key="mainView" navBar={ NavBar} navigationBarStyle={{ position: 'absolute', top: 0}} panHandlers={null} sceneStyle={{ backgroundColor: 'lightgray'}} component={Main} title="Discover Pets" initial={true}/>
               <Scene key="landingPage" component={Landing} title="Landing Page" />
               <Scene key="profileView" component={Profile} title="Your Profile"/>
               <Scene key="ownerView" sceneStyle={{ paddingTop: 65 }} component={Owner} title="Your Pets"/>
->>>>>>> master
               <Scene key="petDetailView" component={PetDetail} title="Pet Details"/>
               <Scene key="matchView"
               navBar={NavBar} component={Match} title="Match View"/>
