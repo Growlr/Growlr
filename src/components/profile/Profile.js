53a76264bef22c8b9573c412d332b7d1f9cdd090
Jon Low <lowjonc@gmail.com>
2017-02-28 19:27:42 -0700

    progress

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -24,6 +24,7 @@ class Profile extends Component {
                       }}>
 
 
+
             {/* --- navbar --- */}
             {/* --- Round image --- */}
             {/* --- edit button --- */}
@@ -47,13 +48,26 @@ class Profile extends Component {
 
                             }}
 
-                    source={{uri: this.props.user.fb_img}}
+                    source={require('../../img/edit_icon.png')}
               />
             </TouchableHighlight>
 
             <Text style={{fontWeight: '600'
                           , fontSize: 24}}>
                           {this.props.user.firstname}</Text>
+
+
+              <TouchableHighlight style={{width: width
+                                          , marginTop: 30
+                                          , alignItems: 'center'}}
+                                          //this button should go to the edit profile view that doesnt exist yet
+                                  onClick={this.clickEditProfile()}
+                                          >
+                <Text style={{color: '#D85050'
+                              , fontWeight: '600'
+                              , fontSize: 18
+                            }}>SETTINGS</Text>
+              </TouchableHighlight>
 
 
 
@@ -65,94 +79,6 @@ class Profile extends Component {
 
 
 
-  //chatImage: {
-  //     width: 58,
-  //     height: 58,
-  //     margin: 15,
-  //     borderRadius: 30,
-  //     marginBottom: 0
-  //   },
-  //   profile: {
-  //     position: 'absolute',
-  //     top: 15,
-  //     left: 10,
-  //     width: 20,
-  //     height: 27
-  //   },
-  //   input: {
-  //     width: 355,
-  //     height: 35,
-  //     borderRadius: 5,
-  //     backgroundColor: '#EEE9E9',
-  //     margin: 10,
-  //     textAlign: 'center'
-  //   },
-  //   match: {
-  //     color: '#D85050',
-  //     fontSize: 18,
-  //     marginLeft: 15,
-  //     marginRight: 15,
-  //     marginTop: 15
-  //   },
-  //   matchImage: {
-  //     width: 80,
-  //     height: 80,
-  //     margin: 15,
-  //     borderRadius: 40
-  //   },
-  //   matchName: {
-  //     marginLeft: 110,
-  //     position: 'absolute',
-  //     top: 30,
-  //     fontSize: 22
-  //   },
-  //   moreInfo: {
-  //     marginLeft: 110,
-  //     position: 'absolute',
-  //     top: 60,
-  //     borderColor: 'gray'
-  //   },
-  //   topBorder: {
-  //     alignItems: 'center',
-  //     borderTopWidth: 0.5,
-  //     borderTopColor: 'lightgray'
-  //   },
-  //   bottomBorder: {
-  //     borderBottomWidth: 0.4,
-  //     borderBottomColor: 'lightgray'
-  //   },
-  //   modalBackground: {
-  //     backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  //     height: 1000
-  //   },
-  //   modalContainer: {
-  //     backgroundColor: '#FFF',
-  //     padding: 32,
-  //     justifyContent: 'center',
-  //     alignItems: 'center',
-  //     marginTop: 250,
-  //     marginBottom: 250,
-  //     marginLeft: 20,
-  //     marginRight: 20,
-  //     borderRadius: 5
-  //   },
-  //   closeModal: {
-  //     color: '#D85050',
-  //     fontWeight: 'bold',
-  //     borderWidth: 1,
-  //     borderColor: '#D85050',
-  //     padding: 5,
-  //     borderRadius: 5,
-  //     position:'absolute',
-  //     top: -12,
-  //     left: -138
-  //   },
-  //   tapCall: {
-  //     color: '#D85050',
-  //     paddingTop: 25,
-  //     fontSize:20
-  //   }
-  // })
 
 
 
